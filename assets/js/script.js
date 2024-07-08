b0463ab37dae7ef0b963e0c32dbd45f7b88d6d62
Erivelton Rodrigues <eriveltonarruda3000@gmail.com>
2024-07-07 21:08:18 -0300

    Cabeçalho 4

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// MENU
+let menuOpener = document.querySelector(".menu-opener");
+let nav = document.querySelector("header nav");
+
+// funcionamento do menu no celular
+menuOpener.addEventListener("click", () => {
+  // se o nav tiver a classe opened, significa que o Menu tá aberto
+  if (nav.classList.contains("opened")) {
+    // então vou fechar
+    nav.classList.remove("opened");
+
+    // quando o nav não tiver a classe opened, removemos o "x"
+    menuOpener.querySelector(".close-icon").style.display = "none";
+    // quando o nav não tiver a classe opened, o hamburguer aparecerá
+    menuOpener.querySelector(".hamburguer-icon").style.display = "flex";
+  } else {
+    // se não tiver a classe opened
+    // ela será adicionada
+    nav.classList.add("opened");
+
+    // Quando o menu estiver aberto, o "x" aparecerá
+    menuOpener.querySelector(".close-icon").style.display = "flex";
+    menuOpener.querySelector(".hamburguer-icon").style.display = "none";
+  }
+});
